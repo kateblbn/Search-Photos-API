3f35763b8033ecd5bf839cfe2cc33b710f688dc7
Kate <kate.pidoni@gmail.com>
2023-07-25 21:36:07 +0200

    styling

--- src/galaryApi/GalaryApi.js
+++ src/galaryApi/GalaryApi.js
@@ -6,6 +6,11 @@ import Template from '../templates/Template';
 function GalaryApi() {
     const [data, setData] = useState([])
     const [srch, setCrch] = useState('music');
+    const [cat, setCat] = useState('cat')
+    const [animals, setanimals] = useState('animals')
+    const [nature, setnature] = useState('nature')
+    const [fashion, setFashion] = useState('fashion')
+
 
     const LINK = `https://api.pexels.com/v1/search?query=${srch}`;
     const LINK_KEY = 'TJofvw42wkBG6ncBAsmWz7yubj8JCFFrCb7JSTReeh5aSw8XynyvNCZ8';
@@ -32,6 +37,18 @@ function GalaryApi() {
         console.log(resultPhotos);
         setCrch(resultPhotos)
     }
+    function searchCat() {
+        setCrch(cat)
+    }
+    function searchAnimals() {
+        setCrch(animals)
+    }
+    function searchNature() {
+        setCrch(nature)
+    }
+    function searchFashion() {
+        setCrch(fashion)
+    }
 
     const getData = data?.map( (el, i) => {
         return <Template src={el.src.medium} alt={el.alt}/>;
@@ -39,6 +56,14 @@ function GalaryApi() {
 
   return (
     <div className='galary'>
+        <div className='galary__btns-cntr'>
+            <div className='galary__btns'>
+                <button  className='galary__btn-cat btn' onClick={searchCat}>Cats</button>
+                <button  className='galary__btn-animal btn' onClick={searchAnimals}>Animals</button>
+                <button  className='galary__btn-nature btn' onClick={searchNature}>Nature</button>
+                <button  className='galary__btn-fashion btn' onClick={searchFashion}>Fashion</button>
+            </div>
+        </div>
         <div className='galary__flex-inpt'>
             <input className='galary__input' placeholder='Music' type='text' onChange={searchPhotos}/>
         </div>
